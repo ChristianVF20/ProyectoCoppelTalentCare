0a6ed608828682258a2bcd56a811899c0fd76bf6
ChristianVF20 <vazquez.cris.2000@gmail.com>
2025-10-17 18:11:31 -0600

    cambios

--- src/Main/Principal.java
+++ src/Main/Principal.java
@@ -18,6 +18,9 @@ public class Principal {
 		String apm;
 		long numCel;
 		
+		System.out.println("Que tal que hace");
+		System.out.println("¿como estas?");
+		
 		Empleados empleado;
 		
 		LogicaMetodos imp = new LogicaMetodos();
